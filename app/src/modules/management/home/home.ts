547f08f2ca75a2ad84294713af53f570c1183329
tiago002pro <tiagobarbosa02@outlook.com>
2021-08-16 08:45:14 -0300

    Botao de novo card na lista

--- app/src/modules/management/home/home.ts
+++ app/src/modules/management/home/home.ts
@@ -2,7 +2,6 @@ import './home.scss'
 
 class HomeController { 
   private titles: any
-  private title: String
   private card: any
   private columns: any
   
@@ -10,9 +9,8 @@ class HomeController {
   
   $onInit() {
     this.titles = ['Stories', 'To Do', 'In Progress', 'Testing', 'Done',]
-    this.title = 'Teste'
     this.columns = [
-      {id: 1, title: 'To Do', card: [{titleCard: 'A Fazer 1...'}, {titleCard: 'A Fazer 2...'}]},
+      {id: 1, title: 'To Do', card: [{titleCard: 'A Fazer 1'}, {titleCard: 'A Fazer 2'}]},
       {id: 2, title: 'In Progres', card: [{titleCard: 'Fazendo'}, {titleCard: 'Fazendo'}, {titleCard: 'Fazendo'}]},
       {id: 3, title: 'Done', card: [{titleCard: 'Terminados'}, {titleCard: 'Terminados'}, {titleCard: 'Terminados'}, {titleCard: 'Terminados'}, {titleCard: 'Terminados'}, {titleCard: 'Terminados'}, {titleCard: 'Terminados'}, {titleCard: 'Terminados'}, {titleCard: 'Terminados'}]}
     ]
