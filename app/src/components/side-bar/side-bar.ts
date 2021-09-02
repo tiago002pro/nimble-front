d5aa8d0db0d3bb8922791026934227e805a9fc47
tiago002pro <tiagobarbosa02@outlook.com>
2021-09-02 17:56:33 -0300

    Novo SideBar

--- app/src/components/side-bar/side-bar.ts
+++ app/src/components/side-bar/side-bar.ts
@@ -4,6 +4,8 @@ import template from './side-bar.html'
 class SidebarController {
     private hide: boolean
     private showdropDowMenu: boolean
+    private black = true
+    private white = false
 
     constructor (
         public $scope,
@@ -28,6 +30,11 @@ class SidebarController {
             this.hide = false
         }
         
+    }
+
+    collorMenu() {
+        this.black = !this.black
+        this.white = !this.white
     }
 }
 
